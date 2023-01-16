7b4c6292b4c0aedbcb8cfd1dddf89e0d6e8298d4
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-15 22:06:06 -0300

    UI

--- PruebaBaseDatos5/Form1.Designer.cs
+++ PruebaBaseDatos5/Form1.Designer.cs
@@ -96,16 +96,18 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 6;
-            this.button1.Text = "button1";
+            this.button1.Text = "Buscar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
+            this.button2.Enabled = false;
             this.button2.Location = new System.Drawing.Point(227, 301);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 7;
-            this.button2.Text = "button2";
+            this.button2.Text = "Borrar";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
--- PruebaBaseDatos5/Form1.cs
+++ PruebaBaseDatos5/Form1.cs
@@ -16,5 +16,10 @@ namespace PruebaBaseDatos5
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
